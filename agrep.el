8ce8eb7f0afdc330eba901a70b228e3b1ebdf26d
willnet <netwillnet@gmail.com>
2013-06-18 14:06:03 +0900

    initial commit

---
+++ agrep.el
@@ -0,0 +1,21 @@
+(defun chomp (str)
+  (replace-regexp-in-string "[\n\r]+$" "" str))
+
+(defun agrep (grep-dir command-args)
+  (interactive
+   (let ((dir (chomp (shell-command-to-string "pwd"))))
+     (list
+      (read-file-name
+       "Directory for agrep: " dir dir t)
+      (read-shell-command
+       "Run ag (like this): "
+       "PAGER='' ag --silent --color --color-match '01;31' --nogroup \"$@\" ")
+      )))
+  (let ((grep-use-null-device nil)
+        (command
+         (format "%s %s"
+                 command-args
+                 grep-dir)))
+    (grep command)))
+
+(provide 'agrep)
