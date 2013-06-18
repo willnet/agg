592ef959cc2ac69917a37d3760e51224c377b630
willnet <netwillnet@gmail.com>
2013-06-18 14:56:30 +0900

    Add meta info

--- agg.el
+++ agg.el
@@ -1,3 +1,23 @@
+;;; agg.el --- a grep command using ag (silver sercher) for emacs
+
+;; Copyright (C) 2013 Shinichi Maeshima
+
+;; Author: Shinichi Maeshima <netwillnet@gmail.com>
+;; URL: http://github.com/willnet/agg
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (defun chomp (str)
   (replace-regexp-in-string "[\n\r]+$" "" str))
 
